2032d771e7dc29b7734b8709fa6368a031d2f308
rezasaputra4250 <129853013+rezasaputra4250@users.noreply.github.com>
2024-05-23 03:13:18 +0700

    perbaikan role

--- controllers/Users.js
+++ controllers/Users.js
@@ -4,7 +4,7 @@ import argon2 from "argon2";
 export const getUsers = async (req, res) => {
     try {
         const response = await User.findAll({
-            attributes: ['userId', 'name', 'email', 'role']
+            attributes: ['uuid', 'name', 'email', 'roleId']
         });
         res.status(200).json(response);
     } catch (error) {
--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -1,27 +1,36 @@
 import User from "../models/UserModel.js";
 
+// Fungsi middleware untuk memeriksa apakah pengguna sudah terautentikasi
 export const verifyUser = async (req, res, next) =>{
+    // Memeriksa apakah ada sesi pengguna yang ada
     if(!req.session.userId){
         return res.status(401).json({msg: "Mohon login ke akun Anda!"});
     }
+    // Mencari pengguna berdasarkan uuid dari sesi
     const user = await User.findOne({
         where: {
             uuid: req.session.userId
         }
     });
+    // Jika pengguna tidak ditemukan, kembalikan respon 404
     if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    // Menambahkan id pengguna dan peran pengguna ke dalam request
     req.userId = user.id;
     req.role = user.role; 
     next();
 }
 
+// Fungsi middleware untuk memeriksa apakah pengguna memiliki peran admin
 export const adminOnly = async (req, res, next) =>{
+    // Mencari pengguna berdasarkan uuid dari sesi
     const user = await User.findOne({
         where: {
             uuid: req.session.userId
         }
     });
+    // Jika pengguna tidak ditemukan, kembalikan respon 404
     if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
-    if(user.role !== "admin") return res.status(403).json({msg: "Akses terlarang"});
+    // Memeriksa apakah pengguna memiliki peran admin
+    if(user.roleId !== 1) return res.status(403).json({msg: "Akses terlarang"});
     next();
 }
