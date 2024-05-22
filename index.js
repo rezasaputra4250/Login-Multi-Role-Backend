1e0ebdaa5d04ffcb244dd8c04f87f6250bac2706
rezasaputra4250 <129853013+rezasaputra4250@users.noreply.github.com>
2024-05-23 04:44:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+import db from "./config/Database.js";
+import SequelizeStore from "connect-session-sequelize";
+import UserRoute from "./routes/UserRoute.js";
+import ProductRoute from "./routes/ProductRoute.js";
+import AuthRoute from "./routes/AuthRoute.js";
 dotenv.config();
 
 const app = express();
 
 const sessionStore = SequelizeStore(session.Store);
+
 const store = new sessionStore({
     db: db
 });
+
+// (async()=>{
+//     await db.sync();
+// })();
 
 app.use(session({
     secret: process.env.SESS_SECRET,
@@ -21,47 +35,13 @@ app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
 }));
-
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173'
-}));
-
 app.use(express.json());
-app.use(roleRoute);
-app.use(userRoute);
-app.use(productRoute);
-app.use(inventoryTransactionRoute);
-app.use(customerRoute);
-app.use(orderRoute);
-app.use(orderDetailRoute);
-app.use(supplierRoute);
-app.use(purchaseRoute);
-app.use(purchaseDetailRoute);
-app.use(authRoute);
+app.use(UserRoute);
+app.use(ProductRoute);
+app.use(AuthRoute);
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
+// store.sync();
 
-db.sync({ force: true }).then(async () => {
-    console.log('Tabel berhasil dibuat.');
-
-    try {
-        // Panggil fungsi seedUsers dan seedRoles sebelum server dimulai
-        console.log('Menjalankan Seeder Roles...');
-        await seedRoles();
-        console.log('Seeder Roles selesai.');
-        console.log('Menjalankan Seeder Users...');
-        await seedUsers();
-        console.log('Seeder Users selesai.');
-
-        app.listen(process.env.APP_PORT, () => {
-            console.log(`Server up and running on port ${process.env.APP_PORT}`);
-        });
-    } catch (error) {
-        console.error('Gagal menjalankan seeder:', error);
-    }
-}).catch(err => {
-    console.error('Gagal membuat tabel:', err);
+app.listen(process.env.APP_PORT, ()=> {
+    console.log('Server up and running...');
 });
